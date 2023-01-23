0c4775e662d27a91e6a20135f59705b6f61c08d9
HadokBjorn <esaubandeira.dev@gmail.com>
2023-01-22 21:34:02 -0500

    sidebar users selection added

--- script.js
+++ script.js
@@ -5,12 +5,15 @@ const main = document.querySelector('.chat-container');
 let nome = document.querySelector('.input-login');
 const mask = document.querySelector('.mask');
 let textoMensagem = ``;
-
+let destino = 'Todos';
+let typeMessage = 'message';
 let usuario = {};
 let listaMensagem = [];
+let userOnline = [];
 
 mensagem.addEventListener('keypress',(e)=>{
     if(e.key === 'Enter'&&nome!==''){
+        e.preventDefault();
         enviarMensagem();
     }
 });
@@ -22,7 +25,7 @@ nome.addEventListener('keypress',(e)=>{
 })
 
 function mudaTamanhoText(item){
-    mensagem.style.height = '80%';
+    mensagem.style.height = '50%';
     mensagem.attributes.placeholder.value = '';
 }
 function atualizaStatus(){
@@ -42,8 +45,10 @@ function enviaNome() {
             container.classList.remove('hidden');
             pageLogin.style.display = 'none';
             dataMensagem();
+            notification();
             setInterval(atualizaStatus, 5000);
             setInterval(dataMensagem,3000);
+            setInterval(chatsAtivo,10000);
         }
     )
     .catch((erro) => {
@@ -66,22 +71,22 @@ function printMensagens(){
 
         if(listaMensagem[i].type === 'status'){
             textoMensagem += `
-            <li class="card-chat status">
+            <li data-test="message" class="card-chat status">
             <p><p class='timer'>(${listaMensagem[i].time})</p> <b>${listaMensagem[i].from}</b> para <b>${listaMensagem[i].to}</b> ${listaMensagem[i].text}</p>
             </li>
             `;
         }
         if(listaMensagem[i].type === 'message'){
             textoMensagem += `
-            <li class="card-chat message">
+            <li data-test="message" class="card-chat message">
             <p><p class='timer'>(${listaMensagem[i].time})</p> <b>${listaMensagem[i].from}</b> para <b>${listaMensagem[i].to}</b> ${listaMensagem[i].text}</p>
             </li>
             `;
         }
-        if(listaMensagem[i].to === nome.value){
+        if(listaMensagem[i].to === nome.value || listaMensagem[i].from === nome.value && listaMensagem[i].type === 'private_message') {
             textoMensagem += `
-            <li class="card-chat private_message">
-            <p><p class='timer'>(${listaMensagem[i].time})</p> <b>${listaMensagem[i].from}</b> para <b>${listaMensagem[i].to}</b> ${listaMensagem[i].text}</p>
+            <li data-test="message" class="card-chat private_message">
+            <p><p class='timer'>(${listaMensagem[i].time})</p> <b>${listaMensagem[i].from}</b> reservadamente para <b>${listaMensagem[i].to}</b> ${listaMensagem[i].text}</p>
             </li>
             `;
         }
@@ -99,9 +104,9 @@ function printMensagens(){
 function enviarMensagem(){
     const texto = {
         from: nome.value,
-        to: "Todos",
+        to: destino,
         text: mensagem.value,
-        type: "message"
+        type: typeMessage
     };
     const enviandoMensagem = axios.post('https://mock-api.driven.com.br/api/v6/uol/messages', texto)
     .then(() => {
@@ -110,10 +115,45 @@ function enviarMensagem(){
         mensagem.style.height = '30%';
         mensagem.attributes.placeholder.value = 'Escreva aqui...';
     })
-    .catch((erro)=>{console.log(erro)})
+    .catch((erro)=>{
+        console.log(erro);
+        window.location.reload()
+    })
 }
 
 function chatsAtivo(){
+
+    let users = `
+    <div onclick="checkMark(this, 'Todos')" data-test="all">
+        <p><ion-icon name="people"></ion-icon> <span class="user-name">Todos</span></p>
+        <div class="check"><ion-icon  name="checkmark-sharp"></ion-icon></div>
+    </div>
+`;
+    const containerUser = document.getElementById('user-online');
+    const ativos = axios.get('https://mock-api.driven.com.br/api/v6/uol/participants')
+    .then((response) => {
+        userOnline = response.data;
+
+        for (let i = 0; i < userOnline.length; i++) {
+            users += `
+            <div onclick="checkMark(this,'${userOnline[i].name}')" data-test="participant">
+                <p><ion-icon name="person-circle-sharp"></ion-icon> <span class="user-name">${userOnline[i].name}</span></p>
+                <div class="check" ><ion-icon data-test="check" name="checkmark-sharp"></ion-icon></div>
+            </div>
+            `
+        }
+        containerUser.innerHTML = users;
+        users = `
+        <div onclick="checkMark(this, 'Todos')" data-test="all">
+            <p><ion-icon name="people"></ion-icon> <span class="user-name">Todos</span></p>
+            <div class="check"><ion-icon  name="checkmark-sharp"></ion-icon></div>
+        </div>
+    `;
+    })
+    
+}
+function openSideBar(){
+    chatsAtivo();
     mask.style.display = 'flex';
 }
 function removerMask(){
@@ -121,18 +161,43 @@ function removerMask(){
 }
 
 
-function checkMark(user){
-    console.log(user)
+function checkMark(user, userName){
+
+    destino = userName;
     const userCheck = user.children[1];
-
-
-    const anterior = document.querySelector('.active');
-    if (anterior !== null){
-        anterior.classList.remove('active');
-        anterior.style.display = 'none';
-
+    const anteriorUser = document.querySelector('#user-online .active');
+    
+    if (anteriorUser !== null){
+        anteriorUser.classList.remove('active');
+        anteriorUser.style.display = 'none';
     }
     userCheck.classList.add('active');
     userCheck.style.display = 'block';
+    notification()
 
+}
+
+function selectedType(type, messageType){
+    const typeCheck = type.children[1];
+
+    typeMessage = messageType;
+    const anteriorType = document.querySelector('#type .active');
+    if (anteriorType !== null){
+        anteriorType.classList.remove('active');
+        anteriorType.style.display = 'none';
+    }
+    typeCheck.classList.add('active');
+    typeCheck.style.display = 'block';
+    notification()
+}
+
+function notification(){
+    const noteUserToMessage = document.getElementById('notificacao');
+
+    if(typeMessage === 'private_message'){
+
+        noteUserToMessage.innerHTML = `Enviando para ${destino} (reservadamente)`
+    }else if (typeMessage === 'message'){
+        noteUserToMessage.innerHTML = `Enviando para ${destino} (publicamente)`;
+    }
 }
